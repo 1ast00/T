7eee55be1c4e43f1c7364a5eb0d0b16f1d1f4798
1ast00 <worud3017@naver.com>
2022-05-23 16:00:27 +0900

    add commit

--- app/src/main/java/com/baewha202041021/test0523_mobile/MainActivity.java
+++ app/src/main/java/com/baewha202041021/test0523_mobile/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /* add function() 추가 기능 */
     }
 }
