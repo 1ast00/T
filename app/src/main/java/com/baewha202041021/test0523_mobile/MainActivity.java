025e09aafa405f0c47d9dc5538b687b916ef1eb6
1ast00 <worud3017@naver.com>
2022-05-23 15:54:15 +0900

    hello world project

---
+++ app/src/main/java/com/baewha202041021/test0523_mobile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.baewha202041021.test0523_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
